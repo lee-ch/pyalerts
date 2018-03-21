f385b5fe6e02363eb603fe3ff7fd60d539fdec90
Lee <lee@network.puthost.com>
2018-03-20 18:29:46 -0600

    Added support in init file to add jobs by adding file in pyalerts/jobs

---
+++ jobs/__init__.py
@@ -0,0 +1,9 @@
+import os
+import sys
+import glob
+
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+modules = glob.glob(cwd+os.sep+"*.py")
+__all__ = [ os.path.basename(f)[:-3] for f in modules if os.path.isfile(f) and not f.endswith('__init__.py')]
+print(__all__)
--- scheduler/worker.py
+++ scheduler/worker.py
@@ -1,10 +1,12 @@
+from __future__ import print_function, absolute_import
 import os
 import sys
 import redis
+
 import pyalerts
+import pyalerts.jobs
 
-
-from pyalerts.jobs.scheduled import *
+from pyalerts.jobs import *
 from pyalerts.scheduler.queue import conn, listen
 from rq import Worker, Queue, Connection
 
